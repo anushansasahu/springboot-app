3a040af2ea1c0180d0e9b2cfe4e57e3af1a50a64
anushansasahu <103962386+anushansasahu@users.noreply.github.com>
2023-06-13 18:39:46 +0530

    Update RestWebController.java

--- src/main/java/com/tcs/angularjs/controller/RestWebController.java
+++ src/main/java/com/tcs/angularjs/controller/RestWebController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
  
 import com.tcs.angularjs.model.Customer;
-//controller class  
+//controller class 1
 @RestController
 public class RestWebController {
     
